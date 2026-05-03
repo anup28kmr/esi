468871f499b4c1a908113b64b28978df9933595b
Melih Arık <meliharik@hotmail.com>
2026-05-03 23:01:19 +0300

    add notification-service and review-service

---
+++ review-service/src/test/java/ee/ut/melih/reviewservice/ReviewServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package ee.ut.melih.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
